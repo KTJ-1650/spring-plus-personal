b48fed39c5ede21961992568551de30c3db5ae1c
KTJ-1650 <xowns1650@gmail.com>
2024-10-06 13:23:58 +0900

    5번문제 일단.. weather 검색만.. IS NULL OR

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -14,8 +14,17 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+
+
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @Query("SELECT t FROM Todo t WHERE t.weather = ?1 ORDER BY t.modifiedAt DESC")
+    Page<Todo> findTodoByWeatherOrderByModifiedAtDesc(Pageable pageable, String weather);
+
+
+    @Query("SELECT t FROM Todo t WHERE (:weather IS NULL OR t.weather = :weather) ORDER BY t.modifiedAt DESC")
+    Page<Todo> findTodoByWeatherOrNull(Pageable pageable, @Param("weather") String weather);
 }
