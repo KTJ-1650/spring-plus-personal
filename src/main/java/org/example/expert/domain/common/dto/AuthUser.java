60df6ca15aed9b8f746f3f1783d4fa10898e032d
KTJ-1650 <xowns1650@gmail.com>
2024-10-09 00:43:41 +0900

    level 2-9

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,17 +2,24 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 public class AuthUser {
 
-    private final Long id;
+    private final Long userId;
     private final String email;
-    private final UserRole userRole;
+    private final UserRole role;
+    private final Collection<? extends GrantedAuthority> authorities;
 
-    public AuthUser(Long id, String email, UserRole userRole) {
-        this.id = id;
+    public AuthUser(Long userId, String email,UserRole role) {
+        this.userId = userId;
         this.email = email;
-        this.userRole = userRole;
+        this.role = role;
+        this.authorities = List.of(new SimpleGrantedAuthority(role.name()));
     }
 }
